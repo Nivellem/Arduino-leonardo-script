b09adf87c4409813ac5a2141e7ce177f25cab13c
Nivellem <84031994+Nivellem@users.noreply.github.com>
2021-11-19 14:56:23 +0100

    Add files via upload

---
+++ press_dell.ino
@@ -0,0 +1,12 @@
+#include <Keyboard.h>
+void setup() {
+  Keyboard.begin();
+  delay(1000);
+  Keyboard.end();
+}
+void loop()
+{
+  Keyboard.press(KEY_DELETE);
+  delay(100);
+  Keyboard.releaseAll();
+}
